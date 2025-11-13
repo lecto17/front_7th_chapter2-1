19d4996a1b57f01236096639a7e555023a043633
lecto17 <khwoo_rpsit@naver.com>
2025-11-13 15:03:45 +0900

    chore: 배포 환경 구분하여 사이트 내 prefix 붙이도록 설정

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    poolOptions: {
-      threads: {
-        singleThread: true,
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/front_7th_chapter2-1/" : "/",
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+      poolOptions: {
+        threads: {
+          singleThread: true,
+        },
       },
     },
-  },
-  base: "/front_7th_chapter2-1/",
+  };
 });
